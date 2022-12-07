14e35536a48b161115e5bd98f07f7ca1f6d1c5e4
Vitaliy Artemov <olivera507224@yandex.ru>
2022-12-07 20:49:48 +0000

    add Department struct

---
+++ C++/sqlite-orm/src/models/department.hpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <fmt/format.h>
+
+using std::string;
+
+
+struct Department{
+public:
+    int id;
+    string name;
+};
+
+template <>
+struct fmt::formatter<Department>: fmt::formatter<fmt::string_view> {
+    template <typename FormatContext>
+    auto format(const Department& dep, FormatContext& ctx) const -> decltype(ctx.out()) {
+        return fmt::format_to(ctx.out(), "Department {{ Id: {:d}, Name: {} }}", dep.id, dep.name);
+    };
+};
