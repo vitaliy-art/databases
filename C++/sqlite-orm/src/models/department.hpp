4adfab07ad7eae97ee7d4b17547d368c610b9ae7
Vitaliy Artemov <olivera507224@yandex.ru>
2023-01-01 17:14:23 +0000

    fix Department

--- C++/sqlite-orm/src/models/department.hpp
+++ C++/sqlite-orm/src/models/department.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <string>
 #include <fmt/format.h>
 
@@ -8,12 +9,15 @@ struct Department{
 public:
     int id;
     string name;
+
+    Department() {}
+    Department(string name) : name(name) {}
 };
 
 template <>
 struct fmt::formatter<Department>: fmt::formatter<fmt::string_view> {
     template <typename FormatContext>
-    auto format(const Department& dep, FormatContext& ctx) const -> decltype(ctx.out()) {
+    auto format(const Department &dep, FormatContext & ctx) const -> decltype(ctx.out()) {
         return fmt::format_to(ctx.out(), "Department {{ Id: {:d}, Name: {} }}", dep.id, dep.name);
     };
 };
