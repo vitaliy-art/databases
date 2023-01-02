cfcc5177e194a1a0238e9f187fbc1356c5afd3d5
Vitaliy Artemov <olivera507224@yandex.ru>
2023-01-02 03:39:09 +0300

    fix Person constructor

--- C++/sqlite-orm/src/models/person.hpp
+++ C++/sqlite-orm/src/models/person.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include <string>
 #include <fmt/format.h>
+#include <boost/lexical_cast.hpp>
+#include <boost/uuid/random_generator.hpp>
+#include <boost/uuid/uuid_io.hpp>
 
 using std::string;
 
@@ -13,13 +16,17 @@ public:
     long birthDate;
 
     Person() {}
-    Person(string name, string address, long birthDate = 0) : name(name), address(address), birthDate(birthDate) {}
 
-    Person(string name, string address, int birthYear, int birthMon, int birthDay) : Person(name, address) {
+    Person(string name, string address, long birthDate = 0) : name(name), address(address), birthDate(birthDate) {
+        boost::uuids::random_generator gen;
+        this->id = boost::lexical_cast<string>(gen());
+    }
+
+    Person(string name, string address, int birth_year, int birth_mon, int birth_day) : Person(name, address) {
         struct tm date = {0};
-        date.tm_year = birthYear - 1900;
-        date.tm_mon = birthMon - 1;
-        date.tm_mday = birthDay;
+        date.tm_year = birth_year - 1900;
+        date.tm_mon = birth_mon - 1;
+        date.tm_mday = birth_day;
         auto epoch = timegm(&date);
         this->birthDate = epoch;
     }
